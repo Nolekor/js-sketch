eb6b8fa364f6932beef963e3bd560148899525ba
Nolekor <pellegrino.nicola90@gmail.com>
2023-01-20 10:49:10 +0100

    add logic for windowload and default

--- app.js
+++ app.js
@@ -11,20 +11,38 @@ function getUserInput() {
     makeGrid(userInput)
 }
 
+window.onload = function() {
+    makeGrid(16);
+  };
 
 function makeGrid(numOfBox) {
     const container = document.querySelector("#container");
     container.style.gridTemplateColumns = `repeat(${numOfBox}, 1fr)`;
     for (let i = 0; i < numOfBox*numOfBox; i++) {
         let square = document.createElement('div')
+        square.style.backgroundColor = "white";
         square.classList.add('box')
+        square.addEventListener('mouseover', changeGridColor)
         container.appendChild(square);
     }
 }
 
+// function noRgbColor() {}
+
+function changeGridColor(e) {
+    let color = `rgb(${[0, 0, 0].map( () => {
+      return Math.floor(Math.random() * 256);
+    }).join(',')})`
+    Object.assign(e.target.style, {
+      backgroundColor : color
+    });
+  }
+  
+
 function resetGrid ()  {
     const container = document.querySelector("#container");
     container.innerHTML = ""
+    makeGrid(16)
 }
 
 
@@ -35,3 +53,7 @@ btnstr.addEventListener('click', getUserInput)
 let btnrs = document.querySelector('#reset')
 btnrs.addEventListener('click', resetGrid)
 
+// let noRGB = document.querySelector("#no-Rgb")
+// let rgbColor = document.querySelector("#rgbColor")
+// noRGB.addEventListener('click', noRgbColor)
+// rgbColor.addEventListener('click', changeGridColor)
